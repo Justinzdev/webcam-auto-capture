3048b0078e06ea03afe654bc0d022d9456e5efb3
godylector <fullyz1532@gmail.com>
2024-01-22 23:37:36 +0700

    update

--- api/app.js
+++ api/app.js
@@ -1,38 +1,55 @@
-const express = require('express');
-const cors = require('cors')
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const app = express();
 const port = 3001;
 
+const axios = require("axios");
+const FormData = require("form-data");
+
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   credential: true,
-  methods: 'GET, POST'
-}
+  methods: "GET, POST",
+};
 
-app.use(cors(corsOptions))
-app.use(express.json({ limit: '10mb' }))
-app.use(bodyParser.json({ limit: '10mb' }));
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "10mb" }));
+app.use(bodyParser.json({ limit: "10mb" }));
 
-app.post('/upload', (req, res) => {
+app.post("/upload", async (req, res) => {
   const { imageSrc } = req.body;
-
   const matches = imageSrc.match(/^data:image\/([A-Za-z-+\/]+);base64,(.+)$/);
-  const imageBuffer = new Buffer.from(matches[2], 'base64');
+  // const imageBuffer = Buffer.from(matches[2], "base64");
+  // const filename = `image_${Date.now()}.png`;
+  // const filePath = `uploads/${filename}`;
 
-  const filename = `image_${Date.now()}.png`;
-  const filePath = `uploads/${filename}`;
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/meter_reading",
+      {
+        imageSrc: matches.input,
+      }
+    );
 
-  fs.writeFile(filePath, imageBuffer, 'base64', (err) => {
-    if (err) {
-      console.error('Error saving the image:', err);
-      res.status(500).send('Error saving the image');
-    } else {
-      console.log('Image saved:', filename);
-      res.send('Image saved');
-    }
-  });
+    console.log(response.data)
+
+    res.send("Image saved and sent to Flask API");
+  } catch (error) {
+    console.error("Error sending the image to Flask API:", error);
+    res.status(500).send("Error sending the image to Flask API");
+  }
+
+  // fs.writeFile(filePath, imageBuffer, "base64", async (err) => {
+  //   if (err) {
+  //     console.error("Error saving the image:", err);
+  //     res.status(500).send("Error saving the image");
+  //   } else {
+  //     console.log("Image saved:", filename);
+      
+  //   }
+  // });
 });
 
 app.listen(port, () => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,92 @@
-import React, { useEffect, useRef } from 'react'
-import Webcam from 'react-webcam'
-import axios from 'axios'
+import React, { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+import axios from "axios";
 
 const App: React.FC = () => {
-     const webcamRef: any = useRef(null)
+  const webcamRef: any = useRef(null);
 
-     const time = 5 // ทุก 5 วินาที
+  const time = 5; // ทุก 5 วินาที
 
-     const captureImage = async () => {
-          const imageSrc = webcamRef.current.getScreenshot();
-          if (imageSrc) {
-               try {
-                    const response = await axios.post('http://localhost:3001/upload', { imageSrc });
-                    console.log(response.data);
-               } catch (error) {
-                    console.error('Error uploading image:', error);
-               }
-          }
-     }  
+  const captureImage = async () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    if (imageSrc) {
+      try {
+        const response = await axios.post("http://localhost:3001/upload", {
+          imageSrc,
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    }
+  };
 
-     useEffect(() => {
-          navigator.mediaDevices.getUserMedia({ video: true })
-          .then((stream) => {
-               if (webcamRef.current) {
-                    webcamRef.current.srcObject = stream;
-               }
-          })
-          .catch((error) => console.error('Error accessing webcam:', error));
+//   const [selectedFile, setSelectedFile]: any = useState(null);
 
-          const captureInterval = setInterval(captureImage, time * 1000)
+//   const handleFileSelect = (event: any) => {
+//     convertToBase64(event.target.files[0]);
+//   };
 
-          return () => {
-               clearInterval(captureInterval);
-               if (webcamRef.current && webcamRef.current.srcObject) {
-                    const tracks = webcamRef.current.srcObject.getTracks();
-                    tracks.forEach((track: any) => track.stop());
-               }
-          };
-     }, []);
+//   const convertToBase64 = (file: any) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onload = () => {
+//       setSelectedFile(reader.result);
+//     };
+//     reader.onerror = (error) => {
+//       console.log("Error converting to base64:", error);
+//     };
+//   };
 
-     return (
-          <div>
-               <Webcam
-                    audio={false}
-                    ref={webcamRef}
-               />
-          </div>
-     );
-}
+//   const handleUpload = async () => {
+//     if (!selectedFile) {
+//       alert("Please select a file first!");
+//       return;
+//     }
 
-export default App
+//     try {
+//       const response = await axios.post(
+//         "http://localhost:3001/upload",
+//         {
+//           imageSrc: selectedFile,
+//         }
+//       );
+//       console.log("File uploaded successfully:", response.data);
+//     } catch (error) {
+//       console.error("Error uploading file:", error);
+//     }
+//   };
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+         if (webcamRef.current) {
+              webcamRef.current.srcObject = stream;
+         }
+    })
+    .catch((error) => console.error('Error accessing webcam:', error));
+    const captureInterval = setInterval(captureImage, time * 1000)
+    return () => {
+         clearInterval(captureInterval);
+         if (webcamRef.current && webcamRef.current.srcObject) {
+              const tracks = webcamRef.current.srcObject.getTracks();
+              tracks.forEach((track: any) => track.stop());
+         }
+    };
+  }, []);
+
+  return (
+//     <div>
+//       <input type="file" onChange={handleFileSelect} required />
+//       <button onClick={handleUpload}>Upload</button>
+//     </div>
+    <div>
+         <Webcam
+              audio={false}
+              ref={webcamRef}
+         />
+    </div>
+  );
+};
+
+export default App;
