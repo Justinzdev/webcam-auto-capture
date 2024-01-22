e344a617b5b6afbbda6cae068b7a2396072d9d3f
godylector <fullyz1532@gmail.com>
2024-01-22 22:39:52 +0700

    update

--- api/app.js
+++ api/app.js
@@ -12,8 +12,8 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.use(express.json())
-app.use(bodyParser.json());
+app.use(express.json({ limit: '10mb' }))
+app.use(bodyParser.json({ limit: '10mb' }));
 
 app.post('/upload', (req, res) => {
   const { imageSrc } = req.body;
